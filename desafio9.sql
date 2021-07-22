d87ba6139a52fa8cdf70ae4c5dc058d5c1a78032
Robertson <robertson.nunes@gmail.com>
2021-07-22 15:56:36 -0300

    desafio 16

--- desafio9.sql
+++ desafio9.sql
@@ -1,3 +1,7 @@
 -- 9 - Mostre todos os valores de notes da tabela purchase_orders que não são nulos.
-SELECT * FROM northwind.purchase_orders
-WHERE notes IS NOT NULL;
+SELECT 
+    *
+FROM
+    northwind.purchase_orders
+WHERE
+    notes IS NOT NULL;
