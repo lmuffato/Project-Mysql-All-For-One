0e9882f3919d315878550fd312f716f86c0232df
hheloisa <hhackenhaar@gmail.com>
2021-07-22 17:02:48 -0300

    Fix: Style

--- desafio21.sql
+++ desafio21.sql
@@ -1,6 +1,3 @@
 INSERT INTO northwind.order_details
 VALUES(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129'),
 (DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129');
-INSERT INTO northwind.order_details
-VALUES(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129'),
-(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129');
