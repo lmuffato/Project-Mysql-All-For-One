c9b6863f3ab6878f345307825db856c5430c2461
renancosta21 <renancosta21@gmail.com>
2021-07-22 16:20:11 -0300

    requisito 3 fix evaluator

--- desafio3.sql
+++ desafio3.sql
@@ -2,5 +2,5 @@ SELECT id FROM northwind.products;
 
 -- https://www.tutorialspoint.com/how-to-get-primary-key-of-a-table-in-mysql--
 -- SHOW INDEX FROM business.TwoOrMorePrimary
-   -- WHERE Key_name = 'PRIMARY';
--- consultei o link acima para descobrir uma query que retornasse a chave primária de uma tabela-- 
+-- WHERE Key_name = 'PRIMARY';
+-- consultei o link acima para descobrir uma query que retornasse a chave primária de uma tabela--
