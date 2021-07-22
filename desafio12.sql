9f145e00f3c1784772c76e3738b194cbc48b2575
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-07-22 17:06:30 -0300

    [refactoring] Refatorando exercicio 12 para utilizar uma logica mais legivel

--- desafio12.sql
+++ desafio12.sql
@@ -1,2 +1,2 @@
 -- Mostre as submitted_date de purchase_orders em que a submitted_date é do dia 26 de abril de 2006.
-SELECT submitted_date FROM `purchase_orders` WHERE DAY(submitted_date)=26 AND MONTH(submitted_date)=04 AND YEAR(submitted_date)=2006;
+SELECT submitted_date FROM `purchase_orders` WHERE DATE(submitted_date)='26-04-2006'
