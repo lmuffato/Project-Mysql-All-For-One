6bba8fe3dbc23fb007f4f40f467890d869b9a821
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-22 18:55:41 -0300

    Feat: desafio14

---
+++ desafio14.sql
@@ -0,0 +1,4 @@
+-- Mostre os supplier_id da purchase_orders em que o supplier_id seja de 1 a 3, incluindo tanto o 1 quanto o 3.
+
+SELECT supplier_id FROM northwind.purchase_orders
+WHERE (supplier_id between 1 AND 3);
