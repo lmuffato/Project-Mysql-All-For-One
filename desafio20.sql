6a3e73c049d9da2e1926532853df254827a86ff6
Robertson <robertson.nunes@gmail.com>
2021-07-22 16:30:12 -0300

    desafio 21

--- desafio20.sql
+++ desafio20.sql
@@ -1,3 +1,3 @@
 -- 20 - Adicione ao order_details uma linha com os seguintes dados: order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL e inventory_id: 129. Obs.: o id deve ser incrementado automaticamente.
-INSERT INTO nortwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, inventory_id) VALUES
+INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, inventory_id) VALUES
 (69, 80, 15.000, 15.0000, 0, 2, NULL, NULL, 129)
--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,6 @@
 -- 9 - Mostre todos os valores de notes da tabela purchase_orders que não são nulos.
 SELECT 
-    *
+    notes
 FROM
     northwind.purchase_orders
 WHERE
