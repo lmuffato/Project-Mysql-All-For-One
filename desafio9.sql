78cbae09d1bf83b13c726aa32f7d9a07055f2975
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-07-22 16:42:03 -0300

    [fix] Faltou aspas no nome de algumas tabelas

--- desafio9.sql
+++ desafio9.sql
@@ -1,2 +1,2 @@
 -- Mostre todos os valores de notes da tabela purchase_orders que não são nulos.
-SELECT notes FROM purchase_orders WHERE notes IS NOT NULL;
+SELECT notes FROM `purchase_orders` WHERE notes IS NOT NULL;
