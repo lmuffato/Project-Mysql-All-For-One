1ab059b3aff49ca9160ffe2746099103894f8d39
MaxRudim <max.hermsdorff@gmail.com>
2021-07-22 18:54:56 -0300

    Correção, lint pedia uma linha ao final do arquivo

--- desafio13.sql
+++ desafio13.sql
@@ -1,2 +1,3 @@
 SELECT supplier_id FROM northwind.purchase_orders
 WHERE supplier_id IN (1, 3);
+
--- desafio18.sql
+++ desafio18.sql
@@ -1,2 +1,3 @@
 SELECT * FROM northwind.purchase_orders
 WHERE supplier_id IN (3) AND status_id IN (2);
+
