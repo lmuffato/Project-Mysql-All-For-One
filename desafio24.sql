59019e814badfd697d748f8933fca82970c806e0
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-07-22 14:54:10 -0300

    ortografia corrigida 24

--- desafio24.sql
+++ desafio24.sql
@@ -1,1 +1,1 @@
-UPDATE northwind.order_details SET discont = 45 WHERE (unit_price > 10.000) AND (id BETWEEN 30 AND 40);
+UPDATE northwind.order_details SET discount = 45 WHERE (unit_price > 10.000) AND (id BETWEEN 30 AND 40);
