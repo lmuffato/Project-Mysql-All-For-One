95ec20852691ab9b2adedad94f0b42caa26c7845
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-22 17:34:25 -0300

    :sparkles: Requirement 20
    20 - Adicione ao order_details uma linha com os seguintes dados: order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL e inventory_id: 129. Obs.: o id deve ser incrementado automaticamente.

---
+++ desafio20.sql
@@ -0,0 +1,13 @@
+-- 20 - Adicione ao order_details uma linha com os seguintes dados: order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL e inventory_id: 129. Obs.: o id deve ser incrementado automaticamente.
+INSERT INTO
+  order_details (
+    order_id,
+    product_id,
+    quantity,
+    unit_price,
+    status_id,
+    date_allocated,
+    inventory_id
+  )
+VALUES
+  (69, 80, 15.000, 15.000, 2, NULL, 129);
