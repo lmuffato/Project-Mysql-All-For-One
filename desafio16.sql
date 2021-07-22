5e9bf4ee575934a1d34eff6992b504d60b1a0cae
LucasMLara <lucasml@live.com>
2021-07-22 15:11:15 -0300

    Filtered Tables Done

---
+++ desafio16.sql
@@ -0,0 +1,1 @@
+SELECT submitted_date FROM northwind.purchase_orders WHERE submitted_date BETWEEN '2006-01-26 00:00:00' and '2006-03-31 00:00:00'; 
---
+++ desafio19.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(shipper_id) AS orders_count FROM northwind.orders WHERE shipper_id = 2 AND  employee_id IN (5, 6); 
