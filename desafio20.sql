5b25f503b83c0b31b8373dedd189407c804b959c
MaxRudim <max.hermsdorff@gmail.com>
2021-07-22 19:35:15 -0300

    correções code climate

--- desafio20.sql
+++ desafio20.sql
@@ -9,3 +9,4 @@ date_allocated,
 purchase_order_id,
 inventory_id)
 VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
+
--- desafio21.sql
+++ desafio21.sql
@@ -10,3 +10,4 @@ purchase_order_id,
 inventory_id)
 VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
 (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
+
