e74c97dea4d56ce138698ed95913dbc55df43575
Flavio Franco <flavio.win@gmail.com>
2021-07-22 16:03:06 -0300

    requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT notes FROM northwind.purchase_orders
+WHERE notes LIKE '%30'
+  OR notes like '%31'
+  OR notes like '%32'
+  OR notes like '%33'
+  OR notes like '%34'
+  OR notes like '%35'
+  OR notes like '%36'
+  OR notes like '%37'
+  OR notes like '%38'
+  OR notes like '%39';
