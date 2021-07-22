1ef69bae8b57b9a6bba1fcc1ce8f247e1c222703
Ana Ventura <anaventura1811@gmail.com>
2021-07-22 19:15:27 -0300

    fix: corrige requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
-SELECT SUM(5 + 6) AS A,
+SELECT (5 + 6) AS A,
 'de' AS Trybe,
-SUM(2 + 8) AS eh;
+(2 + 8) AS eh;
 
 -- Source: https://docs.microsoft.com/pt-br/sql/t-sql/functions/sum-transact-sql?view=sql-server-ver15
