c7876d580320de89773fb3b23d8919443e7e0df0
biazidioti <bzferreira.bio@gmail.com>
2021-07-22 18:32:47 -0300

    desafio 19

---
+++ desafio19.sql
@@ -0,0 +1,3 @@
+/* 19 - Mostre a quantidade de pedidos que foram feitos na tabela `orders` pelo `employee_id` igual a 5 ou 6, e que foram enviados através do método `shipper_id` igual a 2. Chame a coluna de orders_count. */
+SELECT COUNT(*) AS orders_count FROM northwind.orders WHERE employee_id = 5 OR employee_id = 6 AND shipper_id = 2;
+/* SELECT COUNT(shipper_id) AS orders_count FROM northwind.orders WHERE shipper_id = 2 AND  employee_id IN (5, 6); */
