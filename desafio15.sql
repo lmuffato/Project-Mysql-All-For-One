d5317ab33e4e7bb0a2314ac2102e9c4e7d90cce3
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-22 14:43:32 -0400

    fix: desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,2 +1,2 @@
 -- Mostre somente as horas (sem os minutos e os segundos) da submitted_date de todos registros de purchase_orders. Chame essa coluna de submitted_hour.
-SELECT HOUR(submitted_date) AS 'submmited_hour' FROM northwind.purchase_orders;
+SELECT HOUR(submitted_date) AS 'submited_hour' FROM northwind.purchase_orders;
