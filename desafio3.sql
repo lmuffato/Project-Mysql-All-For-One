243a075144bcd8f12929367193514a5ff808a86b
renancosta21 <renancosta21@gmail.com>
2021-07-22 16:15:09 -0300

    requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+SELECT id FROM northwind.products;
+
+-- https://www.tutorialspoint.com/how-to-get-primary-key-of-a-table-in-mysql--
+-- SHOW INDEX FROM business.TwoOrMorePrimary
+   -- WHERE Key_name = 'PRIMARY';
+-- consultei o link acima para descobrir uma query que retornasse a chave primária de uma tabela-- 
