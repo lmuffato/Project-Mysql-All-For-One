5ac20aee40e696089e2984b2fbb9eb8409b813dc
Viegas <mvrdgs@gmail.com>
2021-07-22 16:43:38 -0300

    table manipulating challenges

--- tests/tableManipulationChallenges.spec.js
+++ tests/tableManipulationChallenges.spec.js
@@ -100,7 +100,7 @@ describe('Desafios de manipulação de tabelas', () => {
       });
     });
 
-    describe('23 - Atualize os dados de `discount` da tabela `order_details` para 30 cuja `unit_price` seja menor que 10.0000.', () => {
+    describe.only('23 - Atualize os dados de `discount` da tabela `order_details` para 30 cuja `unit_price` seja menor que 10.0000.', () => {
       it('Verifica o desafio23', async () => {
         const challengeQuery = readFileSync('desafio23.sql', 'utf8').trim();
 
