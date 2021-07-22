1fa7a573aa4c0718630d2fb7c4504b89b9230a83
MaxRudim <max.hermsdorff@gmail.com>
2021-07-22 18:35:46 -0300

    Segundo blocão feito

---
+++ desafio15.sql
@@ -0,0 +1,3 @@
+SELECT HOUR(submitted_date) AS 'submitted_hour' FROM northwind.purchase_orders;
+
+-- ajuda do course: https://app.betrybe.com/course/back-end/introducao-a-sql/filtrando-dados-de-forma-especifica/9464be4d-cfd8-4fd0-9b31-f037b3c62bff/conteudos/62accdf1-a6dc-488b-a437-78c9653ad7bb/encontrando-e-separando-resultados-que-incluem-datas/e56225da-6ec0-4b52-9ad0-be423d80865b?use_case=side_bar
