ba1ba52e265b91a516df325a057d2cab642ed5fc
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-07-22 18:19:39 -0300

    fix: requisiot 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,1 +1,1 @@
-SELECT status_id,supplier_id FROM northwind.purchase_orders WHERE supplier_id = 3 AND status_id = 2;
+SELECT * FROM northwind.purchase_orders WHERE supplier_id = 3 AND status_id = 2;
