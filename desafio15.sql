dbf4e3064a4ed6cd2becd3d09e1ffe02417886e5
Ana Ventura <anaventura1811@gmail.com>
2021-07-22 19:06:06 -0300

    feat: adiciona referências consultadas p/ desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,3 +1,6 @@
 SELECT EXTRACT(HOUR FROM submitted_date)
 AS submitted_hour 
 FROM northwind.purchase_orders;
+
+-- Source:
+-- https://www.devmedia.com.br/forum/como-extrair-somente-a-hora-de-um-campo-datetime/40760
