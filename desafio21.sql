85340b0b2e30b2077e5e5df8eaedb93499d2fc26
hheloisa <hhackenhaar@gmail.com>
2021-07-22 17:02:02 -0300

    Requisito 21 - Adiciona duas linhas - no mesmo INSERT - a tabela order_details

---
+++ desafio21.sql
@@ -0,0 +1,6 @@
+INSERT INTO northwind.order_details
+VALUES(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129'),
+(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129');
+INSERT INTO northwind.order_details
+VALUES(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129'),
+(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129');
