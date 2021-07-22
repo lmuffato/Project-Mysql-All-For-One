c17eef038644f81b0f8d368ce3f947e5315fbbba
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-07-22 19:36:52 -0300

    [fix] Reformatando desafio 22, nao gostei de separar o update em multiplas linhas

--- desafio22.sql
+++ desafio22.sql
@@ -1,3 +1,2 @@
 -- Atualize os dados de discount do order_details para 15. (Não é necessário utilizar o SAFE UPDATE em sua query).
-UPDATE `order_details`
-SET discount=15;
+UPDATE `order_details` SET discount=15;
