b59811151394e422cc8006af0b525c55626ed4ae
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-07-22 16:55:20 +0000

    fix: correct desafio1.sql and desafio12.sql

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 # 1 - Exiba apenas os nomes do produtos na tabela products.
 SELECT
-    *
+    product_name
 FROM
     northwind.products;
--- desafio12.sql
+++ desafio12.sql
@@ -1,6 +1,6 @@
 # 12 - Mostre as submitted_date de purchase_orders em que a submitted_date é do dia 26 de abril de 2006.
 SELECT
-    *
+    submitted_date
 FROM
     northwind.purchase_orders
 WHERE
