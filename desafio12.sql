ebb568f0cefb2a0838afed48e5e1b454de12a29c
lucas-chaves <lcchavesr150@gmail.com>
2021-07-23 14:55:53 -0300

    correcting challenge 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,3 +1,3 @@
 -- challenge 12;
 
-SELECT submitted_date FROM northwind.purchase_orders WHERE DATE(submitted_date) = '2006-04-25';
+SELECT DATE(submitted_date) FROM northwind.purchase_orders WHERE DATE(submitted_date) = '2006-04-25
