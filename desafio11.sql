6fd2f16a2fc456ee0445aea1e8693658f74ffdf6
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 12:10:04 -0300

    refactoring: desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,17 +1,14 @@
 SELECT notes
 FROM northwind.purchase_orders
-WHERE notes IS NOT NULL ORDER BY notes ASC LIMIT 5;
+WHERE notes LIKE '%#3_';
 
--- outra forma de resolver:
-
+-- > Outra forma de fazer a mesma query:
 -- SELECT notes
 -- FROM northwind.purchase_orders
 -- WHERE notes
 -- BETWEEN 'Purchase generated based on Order #30'
 -- AND 'Purchase generated based on Order #39';
-
--- mais uma alternativa:
+-- > Mais uma alternativa:
 -- SELECT notes
 -- FROM northwind.purchase_orders
--- WHERE notes
--- LIKE '%#3_';
+-- WHERE notes IS NOT NULL ORDER BY notes ASC LIMIT 5;
