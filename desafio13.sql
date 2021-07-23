049893d401836aa0e194667e89b0ac89c210a110
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 12:06:04 -0300

    refactoring: desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,8 +1,3 @@
 SELECT supplier_id
 FROM northwind.purchase_orders
-WHERE supplier_id=1 OR supplier_id=3;
-
--- outra forma de resolver:
--- SELECT supplier_id
--- FROM northwind.purchase_orders
--- WHERE supplier_id IN (1, 3);
+WHERE supplier_id IN(1, 3);
