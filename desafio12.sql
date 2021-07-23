0fbb99cd7f0f6bf2e59a9a53e651a920dce9ec39
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 12:07:22 -0300

    refactoring: desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,9 +1,3 @@
 SELECT submitted_date
 FROM northwind.purchase_orders
-WHERE submitted_date
-BETWEEN '2006-04-26 00:00:00' AND '2006-04-26 23:59:59';
-
--- outra forma de resolver:
--- SELECT submitted_date
--- FROM northwind.purchase_orders
--- WHERE DATE(submitted_date) = '2006-04-26';
+WHERE DATE(submitted_date) = '2006-04-26';
