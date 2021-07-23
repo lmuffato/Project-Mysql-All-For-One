e84c9642a4cfd21f46e6dbba11b8fb1b159094e9
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-23 09:21:50 -0300

    Feat: desafio22

---
+++ desafio22.sql
@@ -0,0 +1,4 @@
+-- Atualize os dados de discount do order_details para 15. (Não é necessário utilizar o SAFE UPDATE em sua query).
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE northwind.order_details SET discount='15';
