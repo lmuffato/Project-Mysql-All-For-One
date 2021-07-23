3dbb672d6050f5a2ee24db062bb4f39b48b1bd2e
Ana Ventura <anaventura1811@gmail.com>
2021-07-23 12:12:06 -0300

    refactoring: desafio 19

--- desafio19.sql
+++ desafio19.sql
@@ -1,3 +1,8 @@
 SELECT COUNT(*) AS orders_count
 FROM northwind.orders
-WHERE (employee_id=5 OR employee_id=6) AND shipper_id=2;
+WHERE employee_id IN(5, 6) AND shipper_id=2;
+
+-- Outra forma de fazer a mesma query:
+-- SELECT COUNT(*) AS orders_count
+-- FROM northwind.orders
+-- WHERE (employee_id=5 OR employee_id=6) AND shipper_id=2;
