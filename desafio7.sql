e58836e9cf93fbff63c10109bf220411d69fab59
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-22 21:14:45 -0300

    feat: challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,2 +1,2 @@
--- 7 - Mostre apenas os ids dos 5 últimos registros da tabela products (a ordernação deve ser baseada na coluna id).
+-- 7 -Mostre apenas os ids dos 5 últimos registros da tabela products (a ordernação deve ser baseada na coluna id).
 SELECT id FROM northwind.products ORDER BY id DESC LIMIT 5;
