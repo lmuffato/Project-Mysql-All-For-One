61413a8194ca95412ef3b8f6b028675742e6db9d
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-23 09:46:59 -0300

    Fix: desafios 22, 23 e 24

--- desafio24.sql
+++ desafio24.sql
@@ -1,5 +1,4 @@
 -- Atualize os dados de discount da tabela order_details para 45 cuja unit_price seja maior que 10.0000 e o id seja um número entre 30 a 40. (Não é necessário utilizar o SAFE UPDATE em sua query).
 
-SET SQL_SAFE_UPDATES = 0;
 UPDATE northwind.order_details SET discount = 45
 WHERE unit_price > 10.000 AND id >= 30 AND id <= 40;
