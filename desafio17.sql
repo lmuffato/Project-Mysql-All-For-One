d095762ac947ad5e3f8564af3af5b83f49dc0296
lucas-chaves <lcchavesr150@gmail.com>
2021-07-23 17:30:40 -0300

    correcting challenge 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,4 +1,3 @@
 -- challenge 17;
 
- SELECT id, supplier_id  FROM northwind.purchase_orders  WHERE supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7;
- 
+SELECT id, supplier_id FROM northwind.purchase_orders WHERE supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7;
