736a7d1dbc357ce7ddf5502d9f1b37d40e477e3c
Erick-Marinho <erickgaves@gmail.com>
2021-07-23 19:01:59 -0300

    refatorando desafios 01, 02, 03 e 04 - Inserindo identacao 4 espacos

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,4 @@
-SELECT product_name
-FROM northwind.products;
+SELECT 
+    product_name
+FROM 
+    northwind.products;
--- desafio4.sql
+++ desafio4.sql
@@ -1,2 +1,4 @@
-SELECT COUNT(product_name)
-FROM northwind.products;
+SELECT 
+    COUNT(product_name)
+FROM 
+    northwind.products;
