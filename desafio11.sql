c17a7d3d7c7b3ca5556d2709ef84ba2e46ea2565
Perycles reis <perycles_reis@hotmail.com>
2021-07-22 21:19:06 -0300

    desafio 11 refactoring

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
---Exiba os dados de notes da tabela purchase_orders em que seu valor de "Purchase generated based on Order"
---está entre 30 e 39, incluindo tanto o valor de 30 quanto de 39.
+-- Exiba os dados de notes da tabela purchase_orders em que seu valor de "Purchase generated based on Order"
+-- está entre 30 e 39, incluindo tanto o valor de 30 quanto de 39.
 SELECT notes FROM northwind.purchase_orders
 WHERE notes
 BETWEEN 'Purchase generated based on Order #30'
