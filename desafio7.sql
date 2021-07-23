e9fe403e4d740daabcff1f4e1330bc7404a906ec
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-22 23:42:46 -0300

    fix: codeclimate

--- desafio7.sql
+++ desafio7.sql
@@ -1,2 +1,3 @@
 -- 7 -Mostre apenas os ids dos 5 últimos registros da tabela products (a ordernação deve ser baseada na coluna id).
-SELECT id FROM northwind.products ORDER BY id DESC LIMIT 5;
+SELECT id FROM northwind.products 
+ORDER BY id DESC LIMIT 5;
