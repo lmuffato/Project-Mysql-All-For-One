40c2ac5e57cde4e3050b201728429f156f6f952f
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-23 10:52:07 -0300

    feat: code climat

--- desafio20.sql
+++ desafio20.sql
@@ -8,8 +8,7 @@ INSERT INTO northwind.order_details (
     date_allocated,
     purchase_order_id,
     inventory_id
-  )
+  ) 
 VALUES
-  (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
-  
-  
+  (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129)
+  ; 
