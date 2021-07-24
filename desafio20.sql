78df9f7b24fe51c9603923bf2705cc45301a3e93
w-cypriano <profwellington17@gmail.com>
2021-07-23 21:49:25 -0300

    desafio20

---
+++ desafio20.sql
@@ -0,0 +1,3 @@
+INSERT  INTO northwind.order_details(
+        order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+VALUES(order_id: 69, product_id: 80, quantity: 15.0000, unit_price: 15.0000, discount: 0, status_id: 2, date_allocated: NULL, purchase_order_id: NULL, inventory_id: 129);
