a67a8f936b12289ba389fc20fc0fe75a10fd954b
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-07-24 09:58:51 -0300

    ch 13 till 16 done

---
+++ desafio14.sql
@@ -0,0 +1,2 @@
+SELECT supplier_id FROM northwind.purchase_orders
+WHERE supplier_id BETWEEN 1 and 3;
---
+++ desafio15.sql
@@ -0,0 +1,3 @@
+SELECT HOUR(submitted_date)
+FROM northwind.purchase_orders
+AS submitted_hour;
