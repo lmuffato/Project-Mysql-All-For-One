8730b3738347b83e3235e136f19dcd7ed061a979
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-07-25 16:07:12 -0300

    fix:code climate

--- desafio12.sql
+++ desafio12.sql
@@ -1,3 +1,2 @@
 SELECT submitted_date from northwind.purchase_orders
 WHERE DATE(submitted_date) = '2006-04-26';
- 
--- desafio13.sql
+++ desafio13.sql
@@ -1,3 +1,2 @@
 SELECT supplier_id from northwind.purchase_orders
 WHERE supplier_id IN (3, 5);
-
