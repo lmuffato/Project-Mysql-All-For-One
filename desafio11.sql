e48ea4541797e00b448d3c9165c0134dc860fbca
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-07-25 17:55:04 -0300

    code climate

--- desafio11.sql
+++ desafio11.sql
@@ -3,8 +3,4 @@ SELECT
 FROM
   northwind.purchase_orders
 WHERE notes
-BETWEEN
-  'Purchase generated based on Order #30'
-  AND 'Purchase generated based on Order #39';
-
-  
+BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order #39';
