d7aabd2906366efc43d296d0603e08e9b3041e63
vinicios <soicinivt@gmail.com>
2021-07-26 13:57:42 -0300

    FIX - Desafio 7.

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT * FROM
 (SELECT id FROM northwind.products
-LIMIT 10 OFFSET 39) AS id
+LIMIT 40, 5) AS id
 ORDER BY id;
 -- source: https://www.tutorialspoint.com/get-another-order-after-limit-with-mysql
