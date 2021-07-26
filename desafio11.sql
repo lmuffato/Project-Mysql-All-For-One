8550ecc050fd910569bfb518329e6b4b8d73def8
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-07-25 22:12:53 -0300

    Desafio 11 -Correcao algum bug q nao sei - Apenas Reescrevi o codigo

--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,2 @@
 SELECT notes FROM northwind.purchase_orders
-WHERE notes BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order  #39';
+WHERE notes BETWEEN 'Purchase generated based on Order #30' AND 'Purchase generated based on Order #39';
