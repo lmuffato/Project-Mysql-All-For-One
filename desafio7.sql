b8c20d25dbfcb815045a924427f79f81c2d67222
vinicios <soicinivt@gmail.com>
2021-07-26 15:24:02 -0300

    FIX - Desafio 7. Anoter order in ids.

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,9 @@
-SELECT * FROM
-(SELECT id FROM northwind.products
-LIMIT 40, 5) AS id
-ORDER BY id;
+-- SELECT * FROM
+-- (SELECT id FROM northwind.products
+-- LIMIT 40, 5) AS id
+-- ORDER BY id;
 -- source: https://www.tutorialspoint.com/get-another-order-after-limit-with-mysql
+
+SELECT id FROM northwind.products
+ORDER BY id DESC
+LIMIT 5;
