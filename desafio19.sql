445bee2c8655f0b3054cdf4253fbfe34b686b103
Roberto Natividade <robertonnv@gmail.com>
2021-07-29 02:04:21 -0300

    Desafio 19

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-select count(employee_id) as orders_count, shipper_id from northwind.orders where employee_id in (5,6) and shipper_id in (2);
+select count(employee_id) as 'orders_count' from northwind.orders where employee_id in(5,6) and shipper_id in(2);
