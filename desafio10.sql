333b2a28aebe90e9d52a3399dec560561374aeab
Roberto Natividade <robertonnv@gmail.com>
2021-07-29 00:03:02 -0300

    Desafio 10.1

--- desafio10.sql
+++ desafio10.sql
@@ -1,2 +1,3 @@
-SELECT * FROM northwind.purchase_orders where created_by >= 3 order by created_by desc, id asc;
+SELECT * FROM northwind.purchase_orders where created_by >= 3 order by
+ created_by desc, id asc;
  
