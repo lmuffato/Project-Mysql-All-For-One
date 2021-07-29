932d516b83fd51a9f6007514f2eea90a5696e2c1
William <w.willers@outlook.com>
2021-07-28 21:28:00 -0400

    Desafio 12 ao 20

---
+++ desafio15.sql
@@ -0,0 +1,1 @@
+SELECT DATE_FORMAT(submitted_date, '%h') as submitted_hour FROM northwind.purchase_orders;
---
+++ desafio19.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(id) AS orders_count FROM northwind.orders WHERE employee_id BETWEEN 5 AND 6 AND shipper_id LIKE 2;
