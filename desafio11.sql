cc5655b09fa0671be31ac1f580b0def191ef1327
jepaur5 <jepaur5@gmail.com>
2021-07-29 03:16:21 -0300

    requisito11

---
+++ desafio11.sql
@@ -0,0 +1,16 @@
+SELECT 
+    notes
+FROM
+    northwind.purchase_orders
+WHERE
+    notes BETWEEN (SELECT 
+            notes
+        FROM
+            northwind.purchase_orders
+        WHERE
+            notes LIKE '%30') AND (SELECT 
+            notes
+        FROM
+            northwind.purchase_orders
+        WHERE
+            notes LIKE '%39');
