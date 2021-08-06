9ef0ec620ffe34dc9010ad048311e1a03eb48321
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-08-06 15:52:48 -0300

    Req 17

---
+++ desafio17.sql
@@ -0,0 +1,4 @@
+SELECT id, supplier_id FROM northwind.purchase_orders
+WHERE supplier_id LIKE '1' OR supplier_id LIKE '3' OR supplier_id LIKE '5' OR supplier_id LIKE '7';
+
+-- SELECT id, supplier_id FROM northwind.purchase_orders WHERE supplier_id IN (1, 3, 5, 7);
