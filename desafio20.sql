8421ee01b01f95cefb44060ec555f63e18c7d03d
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-11 15:53:23 -0300

    Desafio 20, fix desafio 19

--- desafio20.sql
+++ desafio20.sql
@@ -1,1 +1,5 @@
-SELECT * FROM sakila.actors;
+INSERT INTO northwind.order_details
+	(order_id, product_id, quantity, unit_price, discount, status_id, inventory_id)
+VALUES
+	(69, 80, 15.0000, 15.0000, 0, 2, 129);
+  
