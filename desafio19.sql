3a497d62df285e58cc20eeb0609632236cb86479
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-12 14:55:48 -0400

    feat: implemented condition and name in challenge19 query

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) FROM northwind.orders WHERE employee_id=3 OR employee_id=6 ;
+SELECT COUNT(*) AS 'orders_count' FROM northwind.orders WHERE (employee_id=3 OR employee_id=6) AND shipper_id=2 ;
