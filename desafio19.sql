6fc8a8cae645f12e42c5ef4d3e1a0acd5f3fe70d
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-12 16:00:24 -0400

    fix: missing endline in challenge20

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,2 @@
-SELECT COUNT(*) AS 'orders_count' FROM northwind.orders WHERE (employee_id=3 OR employee_id=6) AND shipper_id=2 ;
+SELECT COUNT(*) AS 'orders_count' FROM northwind.orders WHERE (employee_id=3 OR employee_id=6) AND shipper_id=2;
+
